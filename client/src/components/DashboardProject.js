030a9a0ec1b306ad638ab63de22cac995ff1fe2f
Ivan Velev <vanecat@gmail.com>
2022-04-25 16:57:28 -0700

    reverting file changes to not break Yui's work

--- client/src/components/DashboardProject.js
+++ client/src/components/DashboardProject.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useRef} from 'react';
 
 const sampleData = {
     "style": "mapbox://styles/hyphae-lab/cl0lex1tp000115qtikua1z4e",
@@ -7,32 +7,18 @@ const sampleData = {
 };
 
 const DashboardProject = ({project, saveProject, deleteProject}) => {
-    const [project_, setProject] = useState(project);
-    useEffect(() => {
-        setProject(project);
-    }, [project]);
-
     const styleInputEl = useRef(project.style);
     const userInputEl = useRef(project.user);
     const tokenInputEl = useRef(project.token);
 
-    const onInputChange = e => {
-        setProject({...project_, [e.target.name]: e.target.value});
-    };
-    const onInputFocus = e => {
-        e.target.select();
-    };
 
-    const onSaveInternal = () => {
-        saveProject(project_);
-    }
     return <div>
         {project.name}
-        <label>Style: <input name='style' ref={styleInputEl} value={project_.style} onChange={onInputChange} onFocus={onInputFocus} /></label>
-        <label>User: <input name='user' ref={userInputEl} value={project_.user} onChange={onInputChange} onFocus={onInputFocus} /></label>
-        <label>Token: <input name='token' ref={tokenInputEl} value={project_.token} onChange={onInputChange} onFocus={onInputFocus} /></label>
+        <label>Style: <input ref={styleInputEl}/></label>
+        <label>User: <input ref={userInputEl}/></label>
+        <label>Token: <input ref={tokenInputEl}/></label>
 
-        <button button='button' onClick={onSaveInternal}>save</button>
+        <button button='button' onClick={saveProject.bind(null, project)}>save</button>
 
         <button button='button' onClick={deleteProject.bind(null, project.id)}>delete</button>
     </div>;
