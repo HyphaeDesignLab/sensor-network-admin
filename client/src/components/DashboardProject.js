16395bc3b1f17cc56914a52276ae48c6f094547b
Yui Davidson <yuidavidson@gmail.com>
2022-04-29 15:12:28 -0700

    Edit InputNumber.js to work with current format
    Edit InputNumber.js and DashboardProject.js to work with current format for the dashboard

--- client/src/components/DashboardProject.js
+++ client/src/components/DashboardProject.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState, useRef} from 'react';
 import InputString from "./InputString";
+import InputNumber from "./InputNumber";
 
 const DashboardProject = ({project, saveProject, deleteProject}) => {
     const [projectInternal, setProjectInternal] = useState(project);
@@ -10,6 +11,7 @@ const DashboardProject = ({project, saveProject, deleteProject}) => {
     return <div>
         <label>Name: <InputString onSave={saveProject} value={projectInternal.name} name='name' /></label>
         <label>Style: <InputString onSave={saveProject} value={projectInternal.style} name='style' /></label>
+        <label>Zoom: <InputNumber onSave={saveProject} value={projectInternal.zoom} name='zoom' /></label>
 
         <button type='button' onClick={deleteProject.bind(null, project.id)}>delete</button>
     </div>;
--- client/src/components/InputNumber.js
+++ client/src/components/InputNumber.js
@@ -1,25 +1,46 @@
 import React, {useState, useEffect} from "react";
 
-export default function InputNumber({value, name}) {
+export default function InputNumber({value, name, onSave}) {
     const [isEditable, setEditable] = useState(false);
+    const [num, setNum] = useState(value);
+    useEffect(() => {
+        setNum(value);
+    }, [value]);
     const handleEditClick = e => {
         setEditable(s => !s);
     }
     const handleCancelClick = e => {
         e.preventDefault();
-        setEditable(false)
+        setEditable(false);
+        setNum(value);
     }
+    const handleSaveClick = e => {
+        onSave( {[name]: num} );
+        setEditable(false);
+    }
+    const handleChange = e => {
+        console.log(e.target.value);
+        setNum(e.target.value);
+    }
+    const handleKeyUp = e => {
+      if (e.code === 'Escape') {
+          setEditable(false);
+          setNewValue(oldValue);
+      }
+  }
 
     return (
         <div>
             {!!name && <strong>{name}: </strong>}
             {isEditable ?
                 <span>
-                    <input type='number' value={value} />
+                    <input type='number' value={num} onChange={handleChange}/>
                     <a href='#' onClick={handleCancelClick}>cancel</a>
+                    <a href='#' onClick={handleSaveClick}
+                    onKeyUp={handleKeyUp}>save</a>
                 </span>
                 :
-                <span onClick={handleEditClick}>{value}</span>
+                <span onClick={handleEditClick}>{num}</span>
             }
         </div>
     );
