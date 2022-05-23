a72238e2c16ff5d945c4c7144fdd1e71c56139e3
Ivan Velev <vanecat@gmail.com>
2022-05-23 16:18:16 -0700

    sensor ids component: added manual "edit" and ocr-parse edit/update

--- client/src/components/sensors/SensorIds.js
+++ client/src/components/sensors/SensorIds.js
@@ -1,21 +1,63 @@
 import React, {useState, useEffect} from 'react';
 import SensorOcrParser from "./SensorOcrParser";
+import InputString from "../InputString";
+
+
+const sensorIdProps = {
+    deveui: {title: 'Dev EUI'},
+    appeui: {title: 'App EUI'},
+    appkey: {title: 'App Key'}
+};
 
 const SensorIdsComponent = ({ids, onSave}) => {
-    const [ids_, setIds] = useState({});
+    const [ids_, setIds] = useState(null);
+
+    const onEditSaved = (ids__) => {
+        setIds({ids_, ...ids__});
+        saveToAws(ids__);
+        setEditing(false);
+    }
+
     const onOcrConfirmed = (ids__) => {
         setIds(ids__);
         saveToAws(ids__);
+        setEditing(false);
     };
 
-    const saveToAws = (sensorIds__) => {
+    useEffect(() => {
+        if (!!ids) {
+            setIds(ids);
+        }
+    }, [ids]);
+
+    const saveToAws = (ids__) => {
         Promise.resolve()
             .then(() => {
-                onSave(ids_);
-            })
+                onSave(ids__);
+            });
     }
+
+    const [isEditing, setEditing] = useState(false);
+    const onEditClick = () => {
+        setEditing(true);
+    };
+
+
     return <div>
-        <SensorOcrParser onConfirmed={onOcrConfirmed} />
+        {!isEditing ?
+            <React.Fragment>
+                {!!ids_ && Object.keys(sensorIdProps).map(sensorId =>
+                <div key={sensorId}>
+                    <span>{sensorIdProps[sensorId].title}</span>:
+                    <InputString path={sensorId} value={ids_[sensorId]} type={'text'} onSave={onEditSaved} hasLabel={false}
+                                 inputStyle={{padding: '5px', fontFamily: 'monospace, sans-serif', letterSpacing: '3px'}}
+                    />
+                </div>)}
+                <button type='button' onClick={onEditClick}>{!!ids_ ? 'Update Ids' : 'Add Ids'}</button>
+            </React.Fragment>
+            :
+            <SensorOcrParser onConfirmed={onOcrConfirmed} />
+        }
     </div>;
 };
 
