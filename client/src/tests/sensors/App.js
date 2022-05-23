66d102641d8407d45ab0ceb30d90cdd55830314e
Ivan Velev <vanecat@gmail.com>
2022-05-23 16:18:33 -0700

    sensor test update

--- client/src/tests/sensors/App.js
+++ client/src/tests/sensors/App.js
@@ -1,10 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import SensorOcrParser from "../../components/sensors/SensorOcrParser";
+import SensorIds from "../../components/sensors/SensorIds";
+
+const sensorIds = Math.random() < .5 ? {
+    deveui: 'asdasdasd',
+    appeui: 'xvxvxcv',
+    appkey: 'dfghdfgh'
+} : null;
 
 function App() {
+    const onSave = () => {
+        alert('Saved!');
+    }
     return (
         <div>
-            <SensorOcrParser />
+            <SensorIds onSave={onSave} ids={sensorIds} />
         </div>
     );
 }
