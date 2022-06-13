ff3984e4d4cb3255636034f22e7386180b5e2fd6
Ivan Velev <vanecat@gmail.com>
2022-06-13 12:54:04 -0700

    sensor photos: image input improvements; make sure that filereader/image-for-canvas detatch from the component's useState setter hooks upon component dismount

--- client/src/components/sensors/ImageInput.js
+++ client/src/components/sensors/ImageInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 
 const getImgDimensionsFitInBox = (img, MAX_WIDTH, MAX_HEIGHT) => {
     var width = img.width;
@@ -17,43 +17,58 @@ const getImgDimensionsFitInBox = (img, MAX_WIDTH, MAX_HEIGHT) => {
     return [width, height];
 };
 
-const ImageInput = ({onLoaded, fitToBox={width: 2400, height: 2400}}) => {
+const ImageInput = ({onLoaded, label, fitToBox={width: 2400, height: 2400}}) => {
 
-    const [imageData, setImageData] = useState('');
     const [isImageDataLoading, setImageDataLoading] = useState('');
+    let imageTypeRef = '';
+    const tmpImageRef = useRef(null);
+    const tmpImageFileReaderRef = useRef(null);
     const handleImageChange = event => {
         if (event.target.files) {
             setImageDataLoading(true);
             let imgFile = event.target.files[0];
-
-            var imgOriginal = document.createElement('img');
-            imgOriginal.onload = function (event) {
-                // RESIZE TO FIT IN BOX
-                const [width, height] = getImgDimensionsFitInBox(imgOriginal, fitToBox.width, fitToBox.height);
-
-                // Dynamically create a canvas element
-                var canvas = document.createElement("canvas");
-                canvas.width = width;
-                canvas.height = height;
-
-                var canvasContext = canvas.getContext("2d");
-                canvasContext.drawImage(imgOriginal, 0, 0, width, height);
-
-                const dataUrl = canvas.toDataURL(imgFile.type); // get base64
-                onLoaded(dataUrl);
-                setImageDataLoading(false);
-            }
-
-            var reader = new FileReader();
-            reader.onload = readerEvent => {
-                imgOriginal.src = readerEvent.target.result;
-            }
-            reader.readAsDataURL(imgFile);
+            imageTypeRef = imgFile.type;
+            tmpImageFileReaderRef.current.readAsDataURL(imgFile);
         }
     }
 
-    return <form>
-        <input type="file" onChange={handleImageChange} />
+    const handleImageLoaded = event => {
+        // RESIZE TO FIT IN BOX
+        const [width, height] = getImgDimensionsFitInBox(tmpImageRef.current, fitToBox.width, fitToBox.height);
+
+        // Dynamically create a canvas element
+        var canvas = document.createElement("canvas");
+        canvas.width = width;
+        canvas.height = height;
+
+        var canvasContext = canvas.getContext("2d");
+        canvasContext.drawImage(tmpImageRef.current, 0, 0, width, height);
+
+        const dataUrl = canvas.toDataURL(imageTypeRef); // get base64
+        onLoaded(dataUrl);
+        setImageDataLoading(false);
+    };
+
+    const handleFileReaderLoad = event => {
+        tmpImageRef.current.src = event.target.result;
+    }
+
+    useEffect(() => {
+
+        tmpImageRef.current = document.createElement('img');
+        tmpImageRef.current.addEventListener('load', handleImageLoaded);
+
+        tmpImageFileReaderRef.current = new FileReader();
+        tmpImageFileReaderRef.current.addEventListener('load', handleFileReaderLoad);
+
+        return () => {
+            tmpImageRef.current.removeEventListener('load', handleImageLoaded);
+            tmpImageFileReaderRef.current.removeEventListener('load', handleFileReaderLoad);
+        }
+    }, []);
+
+    return <form style={{display: 'inline'}}>
+        <label className='button-link'>{label}<input type="file" onChange={handleImageChange} className='offpage' /></label>
         {isImageDataLoading && <div>Loading image...</div>}
         </form>;
 };
