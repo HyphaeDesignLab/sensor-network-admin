93179b6198484083d9f75a6025f420835279b4b3
Ivan Velev <vanecat@gmail.com>
2022-06-20 21:18:51 -0700

    sensor editing: adjusted save methods for location, photos, and ids and name

--- client/src/components/EditSensor.js
+++ client/src/components/EditSensor.js
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react';
 
 import GeoLocator from "./sensors/GeoLocator";
 import SensorPhotos from "./sensors/SensorPhotos";
+import InputString from "./InputString";
+import SensorIds from "./sensors/SensorIds";
 
 const EditSensor = ({sensor, onSave, onCancel}) => {
 
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState(sensor.location);
   const [isEditLocation, setEditLocation] = useState(false);
 
   const handleLocationSet = (lngLat) => {
     setEditLocation(false);
     setLocation(lngLat);
-    return onSave({...sensor, ...lngLat}); // spread the lng/lat into the sensor copy
+    return onSave({...sensor, ...lngLat}); // spread the lng/lat into original object
   };
 
   const handleLocationEdit = () => {
@@ -19,20 +21,27 @@ const EditSensor = ({sensor, onSave, onCancel}) => {
   };
 
   const handlePhotoUpdate = (photos) => {
-    return onSave({...sensor, photos}); // spread the lng/lat into the sensor copy
+    return onSave({...sensor, photos}); // add the photos array to original object
+  };
+
+  const handleNameEdit = (sensorFragment) => {
+    return onSave({...sensor, ...sensorFragment}); // spread the fragment (contains name) into original object
+  };
+
+  const handleIdsEdit = (ids) => {
+    return onSave({...sensor, ids}); // spread the IDs into original object
   };
 
   return <div>
     <div><a href='#' onClick={onCancel}>&lt;&lt; Back to Project</a></div>
 
     <h2>Sensor</h2>
+    <InputString onSave={handleNameEdit} value={sensor.name ? sensor.name : 'New Sensor'} path='name' type='string' isOnlyEditMode={!sensor.id} hasLabel={false} wrapEl='h3'/>
 
-    {/* to add ORC component here */}
-    <div>Take a photo!</div>
-    {/* highlight A's and S's because those are most commonly wrong */}
-    <div>Check to see if the sensor code is correct!</div>
+
     {/* send device id and show either a success of error to the user*/}
-    <div>Register with sensor with AWS!</div>
+    <div>AWS Registration</div>
+    <SensorIds ids={sensor.ids} onSave={handleIdsEdit} />
 
     <h3>Location</h3>
     <div>{!!location && <span>Longitude: {location.lng}, Latitude: {location.lat}</span>} <a href='#' onClick={handleLocationEdit}>{!!location ? 'Edit':'Add Location'}</a></div>
