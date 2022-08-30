60e21f19f84924f7ca13013698a94dcc670aa6d4
Ivan Velev <vanecat@gmail.com>
2022-08-30 22:09:55 +0300

    added restart and stop routines and background vs foreground execution

--- aws-cli-api/check_node_running.sh
+++ aws-cli-api/check_node_running.sh
@@ -1,4 +1,5 @@
 home_dir="$1";
+echo "Start/Restart/Stop the AWS CLI API server"
 if [ ! "$home_dir" ]; then
   echo "need home dir as first param";
   exit;
@@ -8,10 +9,31 @@ running_check="$(ps aux | grep 'node index.js' | grep -v 'grep')"
 if [ "$running_check" ]; then
         echo 'running';
         echo $running_check;
+        if [ "$2" = 'stop' ]; then
+            for pid in $(ps aux | grep 'node index.js' | grep -v 'grep' | sed -E -e 's/[a-z]+[ \t]+([0-9]+).+/\1/'); do
+                  echo "stopping process " $pid;
+                  kill -KILL $pid;
+            done;
+        fi;
+        if [ "$2" = 'restart' ]; then
+                for pid in $(ps aux | grep 'node index.js' | grep -v 'grep' | sed -E -e 's/[a-z]+[ \t]+([0-9]+).+/\1/'); do
+                  echo "stopping process " $pid;
+                  kill -KILL $pid;
+                done;
+                if [ "$3" = 'background' ]; then
+                        sudo node index.js .env &
+                else
+                        sudo node index.js .env
+                fi;
+        fi;
 else
         cd $home_dir;
         echo 'not running';
         if [ "$2" = 'restart' ]; then
-                sudo node index.js .env &
+                if [ "$3" = 'background' ]; then
+                        sudo node index.js .env &
+                else
+                        sudo node index.js .env
+                fi;
         fi
 fi;
