626e69b74fd1482a8adc4df9b2b8f80121e411a1
Ivan Velev <vanecat@gmail.com>
2022-09-09 15:36:34 -0700

    functions: notes on firestore triggers

--- functions/index.js
+++ functions/index.js
@@ -149,3 +149,66 @@ exportApp.post("/sensors/import", (request, response) => {
     });
 });
 exports.exportApp = functions.https.onRequest(exportApp);
+
+
+/*
+firestore trigger function:
+https://firebase.google.com/docs/functions/firestore-events
+
+const functions = require('firebase-functions');
+
+SINGLE DOC
+exports.myFunction = functions.firestore
+  .document('my-collection/{docId}')
+  .onWrite((change, context) => {
+  });
+
+WILDCARD
+// Listen for changes in all documents in the 'users' collection
+exports.useWildcard = functions.firestore
+    .document('users/{userId}')
+    .onWrite((change, context) => {
+      // If we set `/users/marie` to {name: "Marie"} then
+      // context.params.userId == "marie"
+      // ... and ...
+      // change.after.data() == {name: "Marie"}
+    });
+
+
+onWrite(handler: (change: Change<DocumentSnapshot>, context: EventContext) => any): CloudFunction<Change<DocumentSnapshot>>
+onWriteHandler(change, context) {
+
+      // Get an object with the current document value.
+      // If the document does not exist, it has been deleted.
+      const document = change.after.exists ? change.after.data() : null;
+
+      // Get an object representing the document
+      // e.g. {'name': 'Marie', 'age': 66}
+      const newValue = change.after.data();
+
+      // ...or the previous value before this update
+      const previousValue = change.before.data();
+
+      // access a particular field as you would any JS property
+      const name = newValue.name;
+
+      // Then return a promise of a set operation to update the count
+      return change.after.ref.set({
+        name_change_count: count + 1
+      }, {merge: true});
+}
+
+where:
+context.eventType = <string>
+    google.firestore.document.write
+    google.firestore.document.create
+    google.firestore.document.update
+    google.firestore.document.delete
+
+const db = admin.firestore();
+onWriteHandler() {
+    // writing to OTHER doc/collection other than ones that triggered
+    db.doc('some/otherdoc').set({ ... });
+}
+
+*/
