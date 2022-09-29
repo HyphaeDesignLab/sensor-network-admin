4524d2c352b014b5b1bd2e6ef4ee5074a1747693
Ivan Velev <vanecat@gmail.com>
2022-09-29 11:29:32 -0700

    sensor location adjust: make manual/map editing of location BOTH available

--- client/src/components/sensors/GeoLocator.js
+++ client/src/components/sensors/GeoLocator.js
@@ -9,16 +9,17 @@ import InputString from "../InputString";
 
 const GeoLocator = ({onDone, initialValue=null}) => {
   const [coordinates, setCoordinates] = useState(initialValue);
+  const [manualCoordinates, setManualCoordinates] = useState({});
+  const [isCoordinatesChanged, setCoordinatesChanged] = useState(false);
   const [error, setError] = useState('');
-  const [isGetInProgress, setGetInProgress] = useState(false);
-  const handleGet = () => {
-    setGetInProgress(true);
+  const [isGetCurrentLocationInProgress, setGetCurrentLocationInProgress] = useState(false);
+  const handleGetCurrentLocation = () => {
+    setGetCurrentLocationInProgress(true);
     navigator.geolocation.getCurrentPosition(res => {
       setCoordinates({lng: res.coords.longitude, lat: res.coords.latitude, accuracy: res.coords.accuracy});
-      setGetInProgress(false);
+      setGetCurrentLocationInProgress(false);
     }, err => {
       let error = ({1: 'permission deined', 2: 'position unavailable', 3: 'timeout'})[err.code]  + ' ' + err.message;
-      console.log(error);
       setError(error);
     }, {
       maximumAge: 0,
@@ -27,7 +28,7 @@ const GeoLocator = ({onDone, initialValue=null}) => {
     });
   };
 
-  const [isEdit, setEdit] = useState(false);
+  const [isEditingOnMap, setEditingOnMap] = useState(false);
 
   const mapContainer = useRef(null);
   const map = useRef(null);
@@ -62,10 +63,36 @@ const GeoLocator = ({onDone, initialValue=null}) => {
           }
         });
       });
+
+      const setNewCoordinates = () => {
+        const newCoordinates  = map.current.getCenter();
+        map.current.getSource('currentLocation').setData({
+          type: 'Point',
+          coordinates: [newCoordinates.lng, newCoordinates.lat]
+        });
+        setCoordinates({...coordinates, ...newCoordinates});
+        setEditingOnMap(false);
+      }
+      // standard events
+      map.current.on('dragstart',  () => {setEditingOnMap(true);});
+      map.current.on('dragend', setNewCoordinates);
+      // touch events
+      map.current.on('touchstart',  () => {setEditingOnMap(true);});
+      map.current.on('touchcancel',  () => {setEditingOnMap(false);});
+      map.current.on('touchmove', setNewCoordinates);
     }
   }, [coordinates]);
 
-  // let's make sure the timeout used in "handleEdit" can be cancelled, if component is deconstructed
+  useEffect(() => {
+    if (!!initialValue && JSON.stringify(initialValue) === JSON.stringify(coordinates)) {
+      if (isCoordinatesChanged) {
+        setCoordinatesChanged(false);
+      }
+    }
+    setCoordinatesChanged(true);
+  }, [coordinates]);
+
+  // let's make sure the timeout used in "handleEditOrDoneEdit" can be cancelled, if component is deconstructed
   //   as timeout is waiting, to avoid errors
   const setEditOffWithDelayTimeoutId = useRef(null);
   useEffect(() => {
@@ -78,60 +105,51 @@ const GeoLocator = ({onDone, initialValue=null}) => {
     }
   }, []);
 
-  const handleManualEdit = (coordinate) => {
-    const newCoorinates = {...coordinates, ...coordinate};
-    if (newCoorinates.lng && newCoorinates.lat) {
-      setCoordinates(newCoorinates);
-    }
-  };
-  const handleEdit = () => {
-    // going from ON to OFF
-    if (isEdit) {
-      const newCoordinates  = map.current.getCenter();
-      map.current.getSource('currentLocation').setData({
-        type: 'Point',
-        coordinates: [newCoordinates.lng, newCoordinates.lat]
-      });
-      setCoordinates({...coordinates, ...newCoordinates});
-      setEditOffWithDelayTimeoutId.current = setTimeout(() => {
-        setEdit(false);
-      },1500);
-    } else {
-      if (setEditOffWithDelayTimeoutId.current) {
-        clearTimeout(setEditOffWithDelayTimeoutId.current);
-      }
-      map.current.setCenter([coordinates.lng, coordinates.lat]);
-      setEdit(true);
-    }
+  const handleManualEdit = (coordinatesFragment) => {
+    setManualCoordinates({ ...manualCoordinates, ...coordinatesFragment });
   };
 
+  // whenever manual coordinates change, check if
+  useEffect(() => {
+    if (manualCoordinates.lng && manualCoordinates.lat) {
+      if (JSON.stringify(manualCoordinates) !== JSON.stringify(coordinates)) {
+        map.current.setCenter([manualCoordinates.lng, manualCoordinates.lat]);
+      }
+      setCoordinates(manualCoordinates);
+    }
+  }, [manualCoordinates]);
+
   const handleConfirm = () => {
-    onDone({lng: coordinates.lng, lat: coordinates.lat});
+    onDone({lng: coordinates.lng, lat: coordinates.lat})
+        .then(() => {
+          setCoordinatesChanged(false);
+        });
   };
 
   return <div>
-    {!coordinates ?
       <div>
-        <button onClick={handleGet} disabled={isGetInProgress}>{isGetInProgress ? 'Getting location...' : 'Get Current Location'}</button>
-      </div> :
-      <div>
-        <div style={{width: '300px', height: '300px', position: 'relative'}} className='map-container-outer'>
-          <svg style={{width: '40px', position: 'absolute', bottom: 'calc(50% - 5px)', left: 'calc(50% - 20px)', fill: '#ff28c4', display: isEdit ? '':'none'}} viewBox="0 0 100 100">
+        {isCoordinatesChanged && <button onClick={handleConfirm}>Save Location Edits</button>}
+        <div>
+          <strong>Edit Manually</strong><br/>
+          Longitude:
+          <InputString onSave={handleManualEdit} value={!!coordinates ? coordinates.lng:''} path='lng' type='string' hasLabel={false} wrapEl='span' />
+          Latitude:
+          <InputString onSave={handleManualEdit} value={!!coordinates ? coordinates.lat:''} path='lng' type='string' hasLabel={false} wrapEl='span' />
+          {!!coordinates && !!coordinates.accuracy && <span>accuracy: {coordinates.accuracy}</span>}
+        </div>
+
+        <strong>Edit on Map</strong><br/>
+        <div>
+          <button className='link' onClick={handleGetCurrentLocation} disabled={isGetCurrentLocationInProgress}>{isGetCurrentLocationInProgress ? 'Getting location...' : 'Get Current Location'}</button>
+        </div>
+        {!!coordinates && <div style={{width: '300px', height: '300px', position: 'relative'}} className='map-container-outer'>
+          <svg style={{width: '40px', position: 'absolute', bottom: 'calc(50% - 5px)', left: 'calc(50% - 20px)', fill: '#ff28c4', display: isEditingOnMap ? '':'none'}} viewBox="0 0 100 100">
             <path d="M50,89.5c0.32,0,0.62-0.17,0.78-0.43c1.03-1.59,24.88-39.15,24.88-52.91C75.66,22.02,64.15,10.5,50,10.5  c-14.15,0-25.66,11.51-25.66,25.66c0,13.75,23.85,51.32,24.88,52.91C49.38,89.33,49.68,89.5,50,89.5z M33.46,36.16  c0-9.13,7.41-16.55,16.54-16.55c9.13,0,16.54,7.43,16.54,16.55c0,9.11-7.41,16.54-16.54,16.54C40.87,52.7,33.46,45.27,33.46,36.16z">
             </path>
           </svg>
           <div ref={mapContainer} className="map-container" style={{width: '100%', height: '100%'}}>
           </div>
-        </div>
-        <div>
-          Longitude:
-          <InputString onSave={handleManualEdit} value={coordinates.lng} path='lng' type='string' hasLabel={false} />
-          Latitude:
-          <InputString onSave={handleManualEdit} value={coordinates.lat} path='lng' type='string' hasLabel={false} />
-          (accuracy: {coordinates.accuracy})
-        </div>
-        <button onClick={handleEdit}>{isEdit ? 'Done Adjusting':'Adjust'} Location</button>
-        {!isEdit && <button onClick={handleConfirm}>Confirm Location</button>}
+        </div>}
       </div>
     }
     {error ? <div>{error}</div> : null}
