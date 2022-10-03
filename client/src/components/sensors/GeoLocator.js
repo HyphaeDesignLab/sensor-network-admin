f01d8615ea3f0715f8d1fc4c0e1bbd6624071d85
Ivan Velev <vanecat@gmail.com>
2022-10-03 15:38:55 -0700

    sensor location adjust: make coordinates state a HASH; check its properties lng/lat.  make layer/data id same variable

--- client/src/components/sensors/GeoLocator.js
+++ client/src/components/sensors/GeoLocator.js
@@ -17,7 +17,7 @@ const GeoLocator = ({onDone, initialValue=null}) => {
     }
   }, []);
 
-  const [coordinates, setCoordinates] = useState(initialValue);
+  const [coordinates, setCoordinates] = useState({...initialValue});
   const [manualCoordinates, setManualCoordinates] = useState({...initialValue});
   const [isCoordinatesChanged, setCoordinatesChanged] = useState(false);
   const [error, setError] = useState('');
@@ -48,7 +48,12 @@ const GeoLocator = ({onDone, initialValue=null}) => {
   const mapContainer = useRef(null);
   const map = useRef(null);
   useEffect(() => {
-    if (coordinates && !map.current) {
+    if (!coordinates || !coordinates.lng || !coordinates.lat) {
+      return;
+    }
+    const layerId = 'currentLocation';
+
+    if (!map.current) {
       map.current = new mapboxgl.Map({
         container: mapContainer.current,
         style: 'mapbox://styles/mapbox/streets-v11',
@@ -56,7 +61,7 @@ const GeoLocator = ({onDone, initialValue=null}) => {
         zoom: 16
       });
       map.current.on('load', () => {
-        map.current.addSource('currentLocation', {
+        map.current.addSource(layerId, {
           type: 'geojson',
           data: {
             type: 'Point',
@@ -64,9 +69,9 @@ const GeoLocator = ({onDone, initialValue=null}) => {
           }
         });
         map.current.addLayer({
-          id: 'currentLocation',
+          id: layerId,
           type: 'circle',
-          source: 'currentLocation',
+          source: layerId,
           style: {
             type: 'circle',
             layout: {
@@ -81,7 +86,7 @@ const GeoLocator = ({onDone, initialValue=null}) => {
 
       const setNewCoordinates = () => {
         const newCoordinates  = map.current.getCenter();
-        map.current.getSource('currentLocation').setData({
+        map.current.getSource(layerId).setData({
           type: 'Point',
           coordinates: [newCoordinates.lng, newCoordinates.lat]
         });
@@ -148,7 +153,7 @@ const GeoLocator = ({onDone, initialValue=null}) => {
         <div>
           <button className='link' onClick={handleGetCurrentLocation} disabled={isGetCurrentLocationInProgress}>{isGetCurrentLocationInProgress ? 'Getting location...' : 'Get Current Location'}</button>
         </div>
-        {!!coordinates && <div style={{width: '300px', height: '300px', position: 'relative'}} className='map-container-outer'>
+        {!!coordinates && !!coordinates.lng && !!coordinates.lat && <div style={{width: '300px', height: '300px', position: 'relative'}} className='map-container-outer'>
           <svg style={{width: '40px', position: 'absolute', bottom: 'calc(50% - 5px)', left: 'calc(50% - 20px)', fill: '#ff28c4', display: isEditingOnMap ? '':'none'}} viewBox="0 0 100 100">
             <path d="M50,89.5c0.32,0,0.62-0.17,0.78-0.43c1.03-1.59,24.88-39.15,24.88-52.91C75.66,22.02,64.15,10.5,50,10.5  c-14.15,0-25.66,11.51-25.66,25.66c0,13.75,23.85,51.32,24.88,52.91C49.38,89.33,49.68,89.5,50,89.5z M33.46,36.16  c0-9.13,7.41-16.55,16.54-16.55c9.13,0,16.54,7.43,16.54,16.55c0,9.11-7.41,16.54-16.54,16.54C40.87,52.7,33.46,45.27,33.46,36.16z">
             </path>
