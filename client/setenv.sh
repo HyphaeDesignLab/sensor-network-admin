7fea03129ee3b469f900c1206c4b3adf6ceeb9ed
Ivan Velev <vanecat@gmail.com>
2022-10-04 14:53:05 -0700

    fetching firebase SDK config is expensive if re-compiling too frequently

--- client/setenv.sh
+++ client/setenv.sh
@@ -5,4 +5,18 @@ else
   clientEnv='dev';
 fi;
 cp "./src/keys/client.$clientEnv.js" "./src/keys/client.js"
+
+# Check if last mod date is different
+#  AND if env has changed
+#  IF NOT: skip firebase config setting
+if [ $(stat -f '%c' ./src/keys/firebase-config.js) = $(cat ./src/keys/firebase-config.lastmodified-time.txt) ]; then
+  if [ $(cat ./src/keys/env.last-used.txt) = $clientEnv ]; then
+    echo 'no changes to firebase config';
+    exit;
+  fi
+fi
+
 firebase apps:sdkconfig 2>/dev/null | sed -e 's/});/};/' -e 's/firebase.initializeApp({/export default {/' > ./src/keys/firebase-config.js
+
+echo $(stat -f '%c' ./src/keys/firebase-config.js) > ./src/keys/firebase-config.lastmodified-time.txt
+echo $clientEnv > ./src/keys/env.last-used.txt
