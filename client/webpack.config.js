dec19428b3d2747dc9fbbc89636821c943e7a4f3
Ivan Velev <vanecat@gmail.com>
2022-10-05 12:06:54 -0700

    new tests

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,10 +9,11 @@ module.exports = {
   */
   entry: {
     'dashboard':  { import: `${SRC_DIR}/index.jsx`, filename: `dashboard.bundle.js` },
-    //'sensor-test': { import: `${SRC_DIR}/tests/sensors/index.jsx`, filename: 'sensor-test.bundle.js'},
-    // 'test-context-yui': { import: `${SRC_DIR}/tests/context-yui/index.jsx`, filename: 'test-context-yui.bundle.js'},
-    // 'test-context':  { import: `${SRC_DIR}/tests/context/index.jsx`, filename: `test-context.bundle.js` },
-    // 'test-form':  { import: `${SRC_DIR}/tests/form/index.jsx`, filename: `test-form.bundle.js` }
+    // TESTS:
+    //'sensor': { import: `${SRC_DIR}/tests/sensors/index.jsx`, filename: 'tests/sensor.bundle.js'},
+    // 'image-crop': { import: `${SRC_DIR}/tests/image-crop/index.jsx`, filename: 'tests/image-crop.bundle.js'},
+    // 'context':  { import: `${SRC_DIR}/tests/context/index.jsx`, filename: `tests/context.bundle.js` },
+    // 'form':  { import: `${SRC_DIR}/tests/form/index.jsx`, filename: `tests/form.bundle.js` }
   },
   output: {
     path: DIST_DIR
