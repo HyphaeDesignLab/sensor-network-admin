87d9f38c428752dd3a72ec92e8e44a506d31a959
Ivan Velev <vanecat@gmail.com>
2022-10-31 23:04:59 -0700

    client react/webpack compile firebase client sdk config and add functions URL

--- client/setenv.sh
+++ client/setenv.sh
@@ -1,5 +1,8 @@
 clientEnv="$1";
-cp "./src/keys/client/$clientEnv.js" "./src/keys/client.js"
+read -p 'Hard-reset env? (y)es or else leave blank to skip: ' isHardReset
+if [ "_$isHardReset" = "_y" ]; then
+  echo > ./src/keys/firebase/env.txt
+fi;
 
 # Check if last mod date is different
 #  AND if env has changed
@@ -11,5 +14,22 @@ if [ "$(stat -f '%c' ./src/keys/firebase/config.js)" = "$(stat -f '%c' ./src/key
   fi
 fi
 
-firebase apps:sdkconfig 2>/dev/null | sed -e 's/});/};/' -e 's/firebase.initializeApp({/export default {/' > ./src/keys/firebase/config.js
+
 echo $clientEnv > ./src/keys/firebase/env.txt
+
+envFile='.env'
+if [ "$clientEnv" = 'dev' ]; then
+  envFile='.env.local'
+fi;
+
+functionsUrl=$(grep FUNCTIONS_URL < ../functions/$envFile | sed -e 's/FUNCTIONS_URL=//')
+
+echo > ./src/keys/firebase/config.js;
+echo 'export default {' >> ./src/keys/firebase/config.js
+firebase apps:sdkconfig 2>/dev/null | \
+  sed -E -e '/^\s*\/\/.+/d' \
+         -e '/^.*{\s*$/d' \
+         -e '/^.*});\s*$/d' >> ./src/keys/firebase/config.js
+echo ',' >> ./src/keys/firebase/config.js
+echo "\"functionsUrl\":\"$functionsUrl\"" >> ./src/keys/firebase/config.js
+echo '}' >> ./src/keys/firebase/config.js
